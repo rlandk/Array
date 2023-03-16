764b16629372d4311e3e291ff1731c60c354fc58
rlandk <orlandorolland33@gmail.com>
2023-03-16 09:16:57 +0700

    array

---
+++ array.js
@@ -0,0 +1,6 @@
+let namanegara = ['Indonesia', 'Malaysia', 'Singapura', 'Thailand', 'Vietnam', 'Filipina', 'Myanmar', 'Kamboja', 'Laos', 'Brunei'];
+document.writeln('<h2>Nama-nama Negara:</h2>');
+for (let i = 0; i < namanegara.length; i++) {
+    document.writeln(namanegara[i] + '<br>');
+}
+
---
+++ array2.js
@@ -0,0 +1,7 @@
+let bilangan =[2 , 3 , 8, 4, 4]
+let total = 0;
+for (let i = 0; i < bilangan.length; i++) {
+    total += bilangan[i];
+}
+document.writeln('<br><h2>Total Nilai:</h2>');
+document.writeln(total);
